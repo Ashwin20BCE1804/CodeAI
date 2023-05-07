8cc834157f4eebb65037ee24d4500b0831564b29
Ashwin <ashwin.vitcse@gmail.com>
2023-05-07 17:08:11 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://codeai-mwyg.onrender.com', {
+    const response = await fetch('https://project-codeai.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
